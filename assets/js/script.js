c1da6d154aa02f8f3c2f491f7760c0d2565871b6
rhubarb414 <ctophr.dubois@gmail.com>
2023-01-08 20:40:50 -0800

    adds localstorage for score, skeleton for scoreboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
 var content = document.querySelector(".content");
 var beginButton = document.querySelector(".begin-button");
 var beginButtonText = document.querySelector("#begin-button-text");
-var buttons = document.querySelector(".buttons");
+var gameButtons = document.querySelector(".game-buttons");
 var answerButtons = document.querySelectorAll(".answer-button");
 var answerButtonText = document.querySelectorAll("#answer-button-text");
 var endButton = document.querySelector(".end-button");
 var rightWrongEl = document.querySelector(".right-wrong");
 var rightWrongPtag = document.createElement("p");
 var timerEl = document.querySelector("#timer");
-var countdownSeconds = 40;
+var countdownSeconds = 60;
+
+var playerName = document.getElementById("name");
+var saveScoreBtn = document.getElementById("save-score");
 
 var stopTimer = false;
 
@@ -23,6 +26,8 @@ var questions = [
   ["Question 4", "Answer 1", "Answer 2", "Answer 3", "Right Answer"],
 ];
 
+// *** Begin quiz section – all game mechanics ***
+
 // Called when Begin button is clicked
 function startQuiz() {
   //hide Begin button
@@ -36,14 +41,14 @@ function startQuiz() {
   // populate first question
   nextQuestion();
 
+  // start the clock!
   startTimer();
 }
 
-//add event listeners to .buttons div
-buttons.addEventListener("click", function (event) {
+//add event listeners to .game-buttons div
+gameButtons.addEventListener("click", function (event) {
   if (event.target.textContent == "Begin") {
-    //
-    console.log("Let's begin!");
+    return; //keep wrong() from firing at start of the game
   } else if (event.target.textContent == "Play again?") {
     //reload game if player hits "Play again?"
     location.reload();
@@ -91,7 +96,6 @@ buttons.addEventListener("click", function (event) {
 // Call startQuiz when Begin button is clicked
 beginButton.addEventListener("click", startQuiz);
 
-
 function nextQuestion() {
   //add question text
   content.textContent = questions[currentQuestion][0];
@@ -104,24 +108,21 @@ function nextQuestion() {
 }
 
 function gameOver() {
-    //stop timer
-    stopTimer = true;
+  //stop timer
+  stopTimer = true;
 
-    //set score to be the remaining time
-    score = countdownSeconds;
+  //set score to be the remaining time
+  score = countdownSeconds;
 
-    //clear last question from .content
-    content.textContent = ""
-    
-    //update .content to show user their score
-     function showScore() {
-        content.textContent = "You earned a score of " + score;
-    }
-    //wait until timer stops before displaying score
-    setTimeout(showScore, 1000);
+  //clear last question from .content
+  content.textContent = "";
 
-
-  
+  //update .content to show user their score
+  function showScore() {
+    content.textContent = "You earned a score of " + score;
+  }
+  //wait until timer stops before displaying score
+  setTimeout(showScore, 1000);
 
   //show End button
   endButton.setAttribute("style", "display: block;");
@@ -130,55 +131,70 @@ function gameOver() {
   for (let index = 0; index < answerButtons.length; index++) {
     answerButtons[index].setAttribute("style", "display: none;");
   }
-
-  
 }
 
+//show message after correct answer
 function correct() {
-
   rightWrongPtag.textContent = "Correct!";
   rightWrongEl.appendChild(rightWrongPtag);
-  rightWrongEl.setAttribute("style", "display: block; ")
+  rightWrongEl.setAttribute("style", "display: block; ");
   setTimeout(hideRightWrongEl, 1000); // hide message after 1 second
 }
 
+//show message after wrong answer and subtract 10s from timer
 function wrong() {
-
-    // Subtract 10 seconds if there is more than 10 seconds left
-    if (countdownSeconds >10) {
+  // Subtract 10 seconds if there is more than 10 seconds left
+  if (countdownSeconds > 10) {
     countdownSeconds -= 10;
-} else {
+    // otherwise take the timer to zero instead of going negative
+  } else {
     stopTimer = true;
     countdownSeconds = 0;
-}
+  }
   rightWrongPtag.textContent = "Wrong.";
   rightWrongEl.appendChild(rightWrongPtag);
-  rightWrongEl.setAttribute("style", "display: block;")
+  rightWrongEl.setAttribute("style", "display: block;");
   setTimeout(hideRightWrongEl, 1000); // hide message after 1 second
-
 }
 
 // called during correct() and wrong() to re-hide the right and wrong messages.
 function hideRightWrongEl() {
-    rightWrongEl.setAttribute("style", "display; none;");
+  rightWrongEl.setAttribute("style", "display; none;");
 }
-
 
 function startTimer() {
-    var timer = setInterval(function() {
-      countdownSeconds --;
-      timerEl.textContent = "Time Left: " +countdownSeconds;
-  
-      if(countdownSeconds === 0 || stopTimer === true) {
-        // Stop timer
-        clearInterval(timer);
-        // Prevent timer from going below zero since setInterval causes a delay
-        if (countdownSeconds <0){
-            countdownSeconds = 0;
-            timerEl.textContent = "Time Left: 0"
-        }
-        gameOver();
+  var timer = setInterval(function () {
+    countdownSeconds--;
+    timerEl.textContent = "Time Left: " + countdownSeconds;
+
+    if (countdownSeconds === 0 || stopTimer === true) {
+      // Stop timer
+      clearInterval(timer);
+      // Prevent timer from going below zero since setInterval causes a delay
+      if (countdownSeconds < 0) {
+        countdownSeconds = 0;
+        timerEl.textContent = "Time Left: 0";
       }
-  
-    }, 1000);
+      gameOver();
+    }
+  }, 1000);
 }
+
+// *** Begin post-quiz section – scoreboard and reset mechanics ***
+
+function saveScore() {
+  var playerScore = {
+    name: playerName.value,
+    score: score,
+  };
+  localStorage.setItem("playerScore", JSON.stringify(playerScore));
+}
+
+//call saveScore when player clicks save-score button
+saveScoreBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  saveScore();
+});
+
+function addToScoreboard() {}
+function showScoreboard() {}
